f954373f58bc1f2ca2c150b2a50a2a9e55e00311
gorilla-Kim <00ghks22@naver.com>
2019-02-28 21:08:55 +0900

    update

--- likelion_kgu/likelion/admin.py
+++ likelion_kgu/likelion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import likelion
 
 # Register your models here.
+class likelionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'group_number', 'programming_lang']
+    list_display_links = ['id', 'group_number']
+
+admin.site.register(likelion, likelionAdmin)
--- likelion_kgu/likelion/models.py
+++ likelion_kgu/likelion/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class likelion(models.Model):
+    group_number = models.IntegerField(blank=False)
+    programming_lang = models.CharField(max_length=125,default="no language")
